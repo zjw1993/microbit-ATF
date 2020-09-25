cf2086199b0da59627aaf3fce3d4d6ad63a402dc
zhangjinwei <1070394375@qq.com>
2020-09-25 19:46:15 +0800

    a

--- ATF.ts
+++ ATF.ts
@@ -1,7 +1,7 @@
 //% weight=10 color=#e7660b icon="\uf1b9"
 namespace ATF { 
 
-    //% blockId="LiteLed"
+    //% blockId="ATF_LiteLed" block="LiteLed|%pin|value %value"
     //% weight=80
     export function LiteLed(pin: DigitalPin, value: number): void { 
         pins.digitalWritePin(pin, value);
